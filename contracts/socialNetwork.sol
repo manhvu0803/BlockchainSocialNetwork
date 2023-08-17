427c100d495c4d0f41e2b617f22b318e036cf44a
Nguyễn Mạnh Vũ <31726319+manhvu0803@users.noreply.github.com>
2023-08-17 07:39:14 +0700

    Add like

--- contracts/socialNetwork.sol
+++ contracts/socialNetwork.sol
@@ -12,7 +12,7 @@ contract SocialNetwork {
         uint id;
         string content;
         uint timestamp;
-        address author;
+        User author;
     }
 
     struct Comment {
@@ -20,7 +20,19 @@ contract SocialNetwork {
         uint postId;
         string content;
         uint timestamp;
-        address author;
+        User author;
+    }
+
+    struct Like {
+        uint postId;
+        address authorAddress;
+    }
+
+    struct LikeData {
+        bool isLiked;
+        uint timestamp;
+        Post post;
+        User author;
     }
 
     uint postCount;
@@ -33,11 +45,15 @@ contract SocialNetwork {
 
     mapping (uint => Comment) public comments;
 
+    mapping (string => LikeData) public likes;
+
     event NewUser(User account);
 
     event NewPost(Post post);
 
     event NewComment(Comment comment);
+
+    event LikeStatusChange(Like like);
 
     constructor() {
         postCount = 0;
@@ -58,23 +74,37 @@ contract SocialNetwork {
         _;
     }
 
+    modifier requirePost(uint postId) {
+        require(postId <= postCount);
+    }
+
     function registerNewUser(address userAddress, string memory username, string memory avatarUrl) public requireNonEmpty(username) requireNewUser(userAddress) {
         User memory user = User(userAddress, username, avatarUrl);
         users[userAddress] = user;
         emit NewUser(user);
     }
 
-    function createNewPost(address author, string memory content, uint timestamp) public requireAccount(author) {
+    function createNewPost(address authorAddress, string memory content, uint timestamp) public requireAccount(authorAddress) {
         postCount++;
+        User memory author = users[authorAddress];
         Post memory post = Post(postCount, content, timestamp, author);
         posts[postCount] = post;
         emit NewPost(post);
     }
 
-    function addComment(address author, uint postId, string memory content, uint timestamp) public requireAccount(author) {
+    function addComment(address authorAddress, uint postId, string memory content, uint timestamp) public requireAccount(authorAddress) {
         commentCount++;
+        User memory author = users[authorAddress];
         Comment memory comment = Comment(commentCount, postId, content, timestamp, author);
         comments[commentCount] = comment;
         emit NewComment(comment);
+    }
+
+    function toggleLike(address authorAddress, uint postId, uint timestamp) public requireAccount(authorAddress) requirePost(postId) {
+        Like memory like = Like(postId, authorAddress);
+        LikeData memory oldLikeData = likes[like];
+        LikeData memory likeData = LikeData(!oldLikeData.isLiked, timestamp, posts[postId], users[authorAddress]);
+        likes[like] = likeData;
+        emit LikeStatusChange(likeData);
     }
 }
