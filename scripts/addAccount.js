e4fbebf8bee433549a2214b26779741fa2eb7ffd
Nguyễn Mạnh Vũ <31726319+manhvu0803@users.noreply.github.com>
2023-08-19 16:04:10 +0700

    Rework subgraph structure and add reverse lookup fields

--- scripts/addAccount.js
+++ scripts/addAccount.js
@@ -8,18 +8,38 @@ const provider = new ethers.InfuraProvider(network = "sepolia", API_KEY);
 const signer = new ethers.Wallet(PRIVATE_KEY, provider);
 const contractInstance = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
 
+const address = "0xfb809c4cf09BF87283BF1cAd1002077E85715e2f";
+
 async function main() {
     console.log("Sending new account");
-    let tx = await contractInstance.registerNewUser("0xfb809c4cf09BF87283BF1cAd1002077E85715e2f", "testName", "https://fastly.picsum.photos/id/1051/200/300.jpg?hmac=l1jFNN1wGY2r8gVAipnoabHxa9LKBSoOTznSjOYWsUY");
+    let tx = await contractInstance.registerNewUser(address, "testName", "https://fastly.picsum.photos/id/1051/200/300.jpg?hmac=l1jFNN1wGY2r8gVAipnoabHxa9LKBSoOTznSjOYWsUY");
     console.log("Creating new account");
     await tx.wait();
     console.log("New account created");
 
     console.log("Sending new post");
-    tx = await contractInstance.createNewPost("0xfb809c4cf09BF87283BF1cAd1002077E85715e2f", "Hello world", Date.now());
+    tx = await contractInstance.createNewPost(address, "Hello world", Date.now());
     console.log("Creating new post");
     await tx.wait();
     console.log("New post created");
+
+    console.log("Sending new comment");
+    tx = await contractInstance.addComment(address, 1, "Hello world comment", Date.now());
+    console.log("Creating new comment");
+    await tx.wait();
+    console.log("New comment created");
+
+    console.log("Sending new like");
+    tx = await contractInstance.toggleLike(address, 1);
+    console.log("Creating new like");
+    await tx.wait();
+    console.log("New like created");
+
+    console.log("Sending new user info");
+    tx = await contractInstance.updateUserinfo(address, "Updated user name", "https://fastly.picsum.photos/id/1051/200/300.jpg?hmac=l1jFNN1wGY2r8gVAipnoabHxa9LKBSoOTznSjOYWsUY");
+    console.log("Creating new user info");
+    await tx.wait();
+    console.log("New user info created");
 }
 
 main();
--- social-network/src/social-network.ts
+++ social-network/src/social-network.ts
@@ -1,17 +1,55 @@
 import {
+  LikeStatusChanged as LikeStatusChangedEvent,
+  NewComment as NewCommentEvent,
   NewPost as NewPostEvent,
-  NewUser as NewUserEvent
-} from "../generated/SocialNetwork/SocialNetwork"
-import { NewPost, NewUser } from "../generated/schema"
+  NewUser as NewUserEvent,
+  UserInfoChanged as UserInfoChangedEvent
+} from "../generated/SocialNetwork/SocialNetwork";
+import {
+  Like,
+  Comment,
+  Post,
+  User,
+} from "../generated/schema";
+
+export function handleLikeStatusChanged(event: LikeStatusChangedEvent): void {
+  let id = `${event.params.author.userAddress}_${event.params.post.id}`;
+  let entity = Like.load(id);
+
+  if (!entity) {
+    entity = new Like(id);
+  }
+
+  entity.author = event.params.author.userAddress;
+  entity.post = event.params.post.id.toString();
+  entity.isLiked = event.params.isLiked
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleNewComment(event: NewCommentEvent): void {
+  let entity = new Comment(event.params.comment.id.toString());
+  entity.post = event.params.comment.postId.toString();
+  entity.content = event.params.comment.content
+  entity.timestamp = event.params.comment.timestamp
+  entity.author = event.params.comment.author
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
 
 export function handleNewPost(event: NewPostEvent): void {
-  let entity = new NewPost(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.post_id = event.params.post.id
-  entity.post_content = event.params.post.content
-  entity.post_timestamp = event.params.post.timestamp
-  entity.post_author = event.params.post.author
+  let entity = new Post(event.params.post.id.toString());
+  entity.content = event.params.post.content
+  entity.timestamp = event.params.post.timestamp
+  entity.author = event.params.post.author
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
@@ -21,12 +59,26 @@ export function handleNewPost(event: NewPostEvent): void {
 }
 
 export function handleNewUser(event: NewUserEvent): void {
-  let entity = new NewUser(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account_userAddress = event.params.account.userAddress
-  entity.account_username = event.params.account.username
-  entity.account_avatarUrl = event.params.account.avatarUrl
+  let entity = new User(event.params.account.userAddress);
+  entity.username = event.params.account.username
+  entity.avatarUrl = event.params.account.avatarUrl
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleUserInfoChanged(event: UserInfoChangedEvent): void {
+  let entity = User.load(event.params.author.userAddress);
+
+  if (!entity) {
+    entity = new User(event.params.author.userAddress);
+  }
+
+  entity.username = event.params.author.username
+  entity.avatarUrl = event.params.author.avatarUrl
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
